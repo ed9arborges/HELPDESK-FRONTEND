7f137836bf0172d62db766d36a7301e5a4669260
Edgar Borges <ed9arborges@gmail.com>
2025-08-07 19:56:40 +0100

    fonts variants

--- src/components/Authentication/index.tsx
+++ src/components/Authentication/index.tsx
@@ -1,10 +1,12 @@
 import logo from "../../assets/NavHeader.svg"
 import styles from "./styles.module.css"
+import Text from "../text"
 
 export function Authentication() {
   return (
     <div className={`${styles.background} h-screen`}>
       <img src={logo} alt="Logo HelpDesk" />
+      <Text variant="text-xxs">Test Size</Text>
     </div>
   )
 }
