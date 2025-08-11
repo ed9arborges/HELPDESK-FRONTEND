b79c20b4fbf97594be6902366f2c088c42a1f6b7
Edgar Borges <ed9arborges@gmail.com>
2025-08-12 00:21:47 +0100

    badge component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,29 @@ import './App.css'
 import { StrictMode } from 'react'
 import Icon from "./components/icon"
 import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import TrashIcon from "./assets/icons/trash.svg?react"
+import CircleIcon from "./assets/icons/circle-check.svg?react"
+import Badge from "./components/badge"
 
 function App() {
  
 
   return (
     <StrictMode>
-      <Header/>
-      <Authentication/>
-      <Icon svg={SpinnerIcon} className="fill-feedback-done" animate/>
+      
+      <div>
+        <Header/>       
+        <Icon svg={SpinnerIcon} className="fill-feedback-done" animate/>
+        <Icon svg={TrashIcon} className="fill-feedback-done" />
+        <div>
+          <Badge variant="secondary" iconAdd="done">
+         
+          Aberto</Badge>
+
+          <Badge variant="primary" iconAdd="open">Encerrado</Badge>
+        </div>
+        <Authentication/>
+      </div>
     </StrictMode>    
   )
 }
