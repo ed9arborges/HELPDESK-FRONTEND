52f4b5114a8e72c72f6e7f264c553309a01350b6
Edgar Borges <ed9arborges@gmail.com>
2025-08-13 22:06:45 +0100

    button and tag components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-
-import { Header } from './components/Header'
-import {Authentication} from "./components/Authentication"
-import './App.css'
-import { StrictMode } from 'react'
+import { Header } from "./components/Header"
+import { Authentication } from "./components/Authentication"
+import "./App.css"
+import { StrictMode } from "react"
 import Icon from "./components/icon"
+import Button from "./components/button"
+import Tag from "./components/tag"
+import TagTime from "./components/tag-time"
 import SpinnerIcon from "./assets/icons/spinner.svg?react"
 import TrashIcon from "./assets/icons/trash.svg?react"
 import CircleIcon from "./assets/icons/circle-check.svg?react"
-import Tag from "./components/tag"
+import XIcon from "./assets/icons/x.svg?react"
+import PenIcon from "./assets/icons/pen-line.svg?react"
 
 function App() {
- 
-
   return (
     <StrictMode>
-      
-      <div>
-        <Header/>       
-        <Icon svg={SpinnerIcon} className="fill-feedback-done" animate/>
+      <div className="grid gap-10 bg-gray-600">
+        <Header />
+        <Icon svg={SpinnerIcon} className="fill-feedback-done" animate />
         <Icon svg={TrashIcon} className="fill-feedback-done" />
-        <div>
+        <div className="flex gap-6 m-5">
           <Tag variant="new" iconAdd={SpinnerIcon}>
-         
-          Aberto</Tag>
+            Aberto
+          </Tag>
 
           <Tag variant="danger" iconAdd={TrashIcon}></Tag>
-          <Tag variant="success" iconAdd={TrashIcon}>Encerrado</Tag>
+          <Tag variant="success" iconAdd={TrashIcon}>
+            Encerrado
+          </Tag>
         </div>
-        <Authentication/>
+        <div className="flex gap-6 m-5">
+          <Button icon={TrashIcon}>New Button</Button>
+          <Button icon={PenIcon} size="sm">Label</Button>
+          <Button icon={PenIcon} size="sm"/>
+          <Button icon={TrashIcon} variant="secundary" size="sm">New Button</Button>
+          <Button icon={PenIcon} variant="secundary" size="md">Label</Button>
+          <Button icon={TrashIcon} variant="link" size="linksm">New Button</Button>
+          <Button icon={PenIcon} variant="link" size="linkmd">Label</Button>
+        </div>
+
+         <div className="flex gap-6 m-5">
+          <TagTime >Label</TagTime>
+          <TagTime variant="selected" iconAdd={XIcon}>Label</TagTime>
+          <TagTime variant="read">Label</TagTime>
+          
+        </div>
+
+        <Authentication />
       </div>
-    </StrictMode>    
+    </StrictMode>
   )
 }
 
--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-export const textVariants = cva("font-sans leading-[1.4]",{
+export const textVariants = cva("font-sans",{
   variants: {
     variant: {
       "text-xl-bold": "text-2xl font-bold",
@@ -13,8 +13,6 @@ export const textVariants = cva("font-sans leading-[1.4]",{
       "text-xs": "text-xs font-normal",
       "text-xs-bold": "text-xs font-bold",
       "text-xxs": "text-xs font-bold uppercase",
-      
-
     }
   },
   defaultVariants: {
@@ -22,7 +20,7 @@ export const textVariants = cva("font-sans leading-[1.4]",{
   }
 });
 
-interface TextProps extends VariantProps<typeof textVariants>{
+export interface TextProps extends VariantProps<typeof textVariants>{
   as?: keyof React.JSX.IntrinsicElements
   className?: string
   children?: React.ReactNode
