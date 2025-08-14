09e2a896127e1004194e9803199624a8b360ce55
Edgar Borges <ed9arborges@gmail.com>
2025-08-14 22:28:09 +0100

    container component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Tag from "./components/tag"
 import TagTime from "./components/tag-time"
 import InputText from "./components/input"
 import InputSelect from "./components/input-select"
+import Container from "./components/container"
 
 import SpinnerIcon from "./assets/icons/spinner.svg?react"
 import TrashIcon from "./assets/icons/trash.svg?react"
@@ -19,82 +20,84 @@ import AlertIcon from "./assets/icons/circle-alert.svg?react"
 function App() {
   return (
     <StrictMode>
-      <div className="grid gap-10 bg-gray-600">
-        <Header />
-        <Icon svg={SpinnerIcon} className="fill-feedback-done" animate />
-        <Icon svg={TrashIcon} className="fill-feedback-done" />
-        <div className="flex gap-6 m-5">
-          <Tag variant="new" iconAdd={SpinnerIcon}>
-            Aberto
-          </Tag>
+      <Container>
+        <div className="grid gap-10 bg-gray-600">
+          <Header />
+          <Icon svg={SpinnerIcon} className="fill-feedback-done" animate />
+          <Icon svg={TrashIcon} className="fill-feedback-done" />
+          <div className="flex gap-6 m-5">
+            <Tag variant="new" iconAdd={SpinnerIcon}>
+              Aberto
+            </Tag>
 
-          <Tag variant="danger" iconAdd={TrashIcon}></Tag>
-          <Tag variant="success" iconAdd={TrashIcon}>
-            Encerrado
-          </Tag>
+            <Tag variant="danger" iconAdd={TrashIcon}></Tag>
+            <Tag variant="success" iconAdd={TrashIcon}>
+              Encerrado
+            </Tag>
+          </div>
+          <div className="flex gap-6 m-5">
+            <Button icon={TrashIcon}>New Button</Button>
+            <Button icon={PenIcon} size="sm">
+              Label
+            </Button>
+            <Button icon={PenIcon} size="sm" />
+            <Button icon={TrashIcon} variant="secundary" size="sm">
+              New Button
+            </Button>
+            <Button icon={PenIcon} variant="secundary" size="md">
+              Label
+            </Button>
+            <Button icon={TrashIcon} variant="link" size="linksm">
+              New Button
+            </Button>
+            <Button icon={PenIcon} variant="link" size="linkmd">
+              Label
+            </Button>
+          </div>
+
+          <div className="flex gap-6 m-5">
+            <TagTime>Label</TagTime>
+            <TagTime variant="selected" iconAdd={XIcon}>
+              Label
+            </TagTime>
+            <TagTime variant="read">Label</TagTime>
+          </div>
+
+          <div className="flex gap-6 m-5">
+            <InputText
+              placeholder="Your Text"
+              helperText="Please enter a Text"
+              error={false}
+              label="label"
+              helperIcon={AlertIcon}
+            />
+
+            <InputText
+              placeholder="Your email"
+              helperText="Please enter a valid email"
+              error={true}
+              label="label"
+              helperIcon={AlertIcon}
+            />
+          </div>
+
+          <div className="flex gap-6 m-5">
+            <InputSelect
+              helperText="Please select one"
+              label="Label"
+              helperIcon={AlertIcon}
+            />
+            <InputSelect
+              helperText="Please select one valid option"
+              error={true}
+              label="Label"
+              helperIcon={AlertIcon}
+            />
+          </div>
+
+          <Authentication />
         </div>
-        <div className="flex gap-6 m-5">
-          <Button icon={TrashIcon}>New Button</Button>
-          <Button icon={PenIcon} size="sm">
-            Label
-          </Button>
-          <Button icon={PenIcon} size="sm" />
-          <Button icon={TrashIcon} variant="secundary" size="sm">
-            New Button
-          </Button>
-          <Button icon={PenIcon} variant="secundary" size="md">
-            Label
-          </Button>
-          <Button icon={TrashIcon} variant="link" size="linksm">
-            New Button
-          </Button>
-          <Button icon={PenIcon} variant="link" size="linkmd">
-            Label
-          </Button>
-        </div>
-
-        <div className="flex gap-6 m-5">
-          <TagTime>Label</TagTime>
-          <TagTime variant="selected" iconAdd={XIcon}>
-            Label
-          </TagTime>
-          <TagTime variant="read">Label</TagTime>
-        </div>
-
-        <div className="flex gap-6 m-5">
-          <InputText
-            placeholder="Your Text"
-            helperText="Please enter a Text"
-            error={false}
-            label="label"
-            helperIcon={AlertIcon}
-          />
-
-          <InputText
-            placeholder="Your email"
-            helperText="Please enter a valid email"
-            error={true}
-            label="label"
-            helperIcon={AlertIcon}
-          />
-        </div>
-
-         <div className="flex gap-6 m-5">
-          <InputSelect            
-            helperText="Please select one"          
-            label="Label"
-            helperIcon={AlertIcon}
-          />
-          <InputSelect            
-            helperText="Please select one valid option"
-            error={true}
-            label="Label"
-            helperIcon={AlertIcon}
-          />
-        </div>
-
-        <Authentication />
-      </div>
+      </Container>
     </StrictMode>
   )
 }
