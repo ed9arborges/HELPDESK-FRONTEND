3aeae03901db598451f3c3fa25bf505326b68022
Edgar Borges <ed9arborges@gmail.com>
2025-08-18 09:10:49 +0100

    page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,18 @@
-import { Header } from "./components/Header"
-import { Authentication } from "./components/Authentication"
-import "./App.css"
-import { StrictMode } from "react"
-import Icon from "./components/icon"
-import Button from "./components/button"
-import Tag from "./components/tag"
-import TagTime from "./components/tag-time"
-import InputText from "./components/input"
-import InputSelect from "./components/input-select"
-import Container from "./components/container"
-
-import SpinnerIcon from "./assets/icons/spinner.svg?react"
-import TrashIcon from "./assets/icons/trash.svg?react"
-import CircleIcon from "./assets/icons/circle-check.svg?react"
-import XIcon from "./assets/icons/x.svg?react"
-import PenIcon from "./assets/icons/pen-line.svg?react"
-import AlertIcon from "./assets/icons/circle-alert.svg?react"
+import { BrowserRouter, Routes, Route } from "react-router"
+import PageComponents from "./pages/page-components"
+import LayoutMain from "./pages/layout-main"
+import PageHome from "./pages/page-home"
 
 function App() {
   return (
-    <StrictMode>
-      <Container>
-        <div className="grid gap-10 bg-gray-600">
-          <Header />
-          <Icon svg={SpinnerIcon} className="fill-feedback-done" animate />
-          <Icon svg={TrashIcon} className="fill-feedback-done" />
-          <div className="flex gap-6 m-5">
-            <Tag variant="new" iconAdd={SpinnerIcon}>
-              Aberto
-            </Tag>
-
-            <Tag variant="danger" iconAdd={TrashIcon}></Tag>
-            <Tag variant="success" iconAdd={TrashIcon}>
-              Encerrado
-            </Tag>
-          </div>
-          <div className="flex gap-6 m-5">
-            <Button icon={TrashIcon}>New Button</Button>
-            <Button icon={PenIcon} size="sm">
-              Label
-            </Button>
-            <Button icon={PenIcon} size="sm" />
-            <Button icon={TrashIcon} variant="secundary" size="sm">
-              New Button
-            </Button>
-            <Button icon={PenIcon} variant="secundary" size="md">
-              Label
-            </Button>
-            <Button icon={TrashIcon} variant="link" size="linksm">
-              New Button
-            </Button>
-            <Button icon={PenIcon} variant="link" size="linkmd">
-              Label
-            </Button>
-          </div>
-
-          <div className="flex gap-6 m-5">
-            <TagTime>Label</TagTime>
-            <TagTime variant="selected" iconAdd={XIcon}>
-              Label
-            </TagTime>
-            <TagTime variant="read">Label</TagTime>
-          </div>
-
-          <div className="flex gap-6 m-5">
-            <InputText
-              placeholder="Your Text"
-              helperText="Please enter a Text"
-              error={false}
-              label="label"
-              helperIcon={AlertIcon}
-            />
-
-            <InputText
-              placeholder="Your email"
-              helperText="Please enter a valid email"
-              error={true}
-              label="label"
-              helperIcon={AlertIcon}
-            />
-          </div>
-
-          <div className="flex gap-6 m-5">
-            <InputSelect
-              helperText="Please select one"
-              label="Label"
-              helperIcon={AlertIcon}
-            />
-            <InputSelect
-              helperText="Please select one valid option"
-              error={true}
-              label="Label"
-              helperIcon={AlertIcon}
-            />
-          </div>
-
-          <Authentication />
-        </div>
-      </Container>
-    </StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/components" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/layout-main.tsx
@@ -0,0 +1,29 @@
+import { NavLink, Outlet } from "react-router";
+import Container from "../components/container";
+import Text from "../components/text";
+
+
+export default function LayoutMain() {
+  return <>
+    <Container as="header" className="mt-3 md:mt-20">
+        Hello World! - HEADER
+    </Container>
+    <main className="mt-4 md:mt-8">
+      <Outlet />
+    </main>
+    <footer className="my-5 md:my-10">
+      <nav className="flex items-center justify-center gap-4">
+        <NavLink to="/">
+          <Text variant="text-sm-bold" className="text-gray-300">
+            Main
+          </Text>
+        </NavLink>
+        <NavLink to="/components">
+          <Text variant="text-sm-bold" className="text-gray-300">
+            Components
+          </Text>
+        </NavLink>
+      </nav>
+    </footer>
+  </>
+}
