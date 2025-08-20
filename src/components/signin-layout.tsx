efd119c03389c82f2cccc57bb412b75307a6adc3
Edgar Borges <ed9arborges@gmail.com>
2025-08-20 15:26:07 +0100

    signin onsubmit useState

--- src/components/signin-layout.tsx
+++ src/components/signin-layout.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react"
 import InputText from "./input"
 
 import Button from "./button"
 import AuthSectionContainer from "./auth-section-container"
 
 export function SignInLayout() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Handle sign-in logic here ##############
+  }
+
   return (
     <AuthSectionContainer title="Portal Access" description="Log in to access your account." className="w-full md:max-w-[25rem] flex flex-col mt-8 ">
-      <form className="">
+      <form className="" onSubmit={handleSubmit}>
         <InputText label="Email" type="email" className="mb-4" />
         <InputText label="Password" type="password" />
-        <Button variant="primary" className="mt-10 w-full">
+        <Button type="submit" variant="primary" className="mt-10 w-full">
           Sign In
         </Button>
       </form>
